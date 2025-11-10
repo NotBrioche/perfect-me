0d372de49ea5fc04b00d717d78a608c26b301d18
NotBrioche <fabian.famille08@gmail.com>
2025-11-10 21:42:14 +0100

    Home page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ConfirmButton from '@/components/ConfirmButton.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="flex justify-center">
+    <div class="flex justify-center items-center flex-col gap-y-8">
+      <h1>Test de pureté</h1>
+      <ConfirmButton text="Commencer" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/ConfirmButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps(['text'])
+</script>
+
+<template>
+  <div
+    class="hover:cursor-pointer bg-green-700 hover:bg-green-600 rounded-lg p-2 font-bold select-none active:bg-green-500"
+  >
+    {{ text }}
+  </div>
+</template>
